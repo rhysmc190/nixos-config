a20bf0546dabdd00d17dbfc0c7f5de13be34be03
rhysmc190 <rhysmc190@gmail.com>
2024-03-23 00:16:48 +1100

    enable fingerprint service

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,9 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # fingerprint service
+  services.fprintd.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
