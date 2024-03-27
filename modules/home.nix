dc635caf784bf4ed6a0542aacfb9fb632346c1ae
rhysmc190 <rhysmc190@gmail.com>
2024-03-27 22:20:09 +1100

    remove alacritty

--- modules/home.nix
+++ modules/home.nix
@@ -98,21 +98,6 @@
     };
   };
 
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
-
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
