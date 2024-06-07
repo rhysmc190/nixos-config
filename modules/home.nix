03dfdde3b670285c1f352937a24dc18f6d662db4
rhysmc190 <rhysmc190@gmail.com>
2024-06-07 15:57:26 +1000

    add steam

--- modules/home.nix
+++ modules/home.nix
@@ -89,6 +89,12 @@
     };
   };
 
+  programs.steam = {
+    enable = true;
+    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   # starship - an customizable prompt for any shell
   programs.starship = {
     enable = true;
