7bbb644d804ccad54cf40031430f01e2890e91ad
rhysmc190 <rhysmc190@gmail.com>
2025-10-07 23:33:22 +1100

    update, remove virtualisation.libvirtd.qemu.ovmf - now available by default

--- virt.nix
+++ virt.nix
@@ -16,8 +16,6 @@
       enable = true;
       qemu = {
         swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
         vhostUserPackages = with pkgs; [ virtiofsd ];
       };
     };
