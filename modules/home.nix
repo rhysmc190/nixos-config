6526f7d4e2659f1ea1a12a8f2720c0d4b4e02a77
rhysmc190 <rhysmc190@gmail.com>
2025-12-12 23:44:30 +1100

    add claude code statusline file

--- modules/home.nix
+++ modules/home.nix
@@ -12,20 +12,11 @@ in
   home.username = "rhys";
   home.homeDirectory = "/home/rhys";
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
+  # Link Claude Code statusline script
+  home.file.".claude/statusline.sh" = {
+    source = ../files/statusline.sh;
+    executable = true;
+  };
 
   services.mpris-proxy.enable = true;
 
