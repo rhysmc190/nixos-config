28ab25377c2ae392f6eb5a3ad84f0e75b17b9565
rhysmc190 <rhysmc190@gmail.com>
2026-02-16 08:55:16 +1100

    tmux config add resurrect and continuum, cosmetics

--- modules/home.nix
+++ modules/home.nix
@@ -74,6 +74,29 @@ in
   programs.tmux = {
     enable = true;
     mouse = true;
+    escapeTime = 0;
+    historyLimit = 50000;
+    terminal = "tmux-256color";
+    extraConfig = ''
+      set -ag terminal-overrides ",ghostty:RGB"
+    '';
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-capture-pane-contents 'on'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '10'
+        '';
+      }
+      {
+        plugin = tokyo-night-tmux;
+        extraConfig = "set -g @tokyo-night-tmux_theme storm";
+      }
+    ];
   };
 
   programs.git = {
