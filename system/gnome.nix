e34ea37badcd2eda7bb1f08d9210b70719a46b79
rhysmc190 <rhysmc190@gmail.com>
2026-02-17 09:45:55 +1100

    fix battery health charging error

--- system/gnome.nix
+++ system/gnome.nix
@@ -1,5 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, settings, ... }:
+let
+  bhcResources = "${pkgs.gnomeExtensions.battery-health-charging}/share/gnome-shell/extensions/Battery-Health-Charging@maniacx.github.com/resources";
+in
 {
+  # Battery Health Charging extension requires its helper script and a polkit rule
+  system.activationScripts.batteryHealthCharging.text = ''
+    mkdir -p /usr/local/bin
+    install -m 0755 ${bhcResources}/batteryhealthchargingctl /usr/local/bin/batteryhealthchargingctl-${settings.username}
+  '';
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if (action.id === "org.freedesktop.policykit.exec" &&
+            action.lookup("program") === "/usr/local/bin/batteryhealthchargingctl-${settings.username}" &&
+            subject.user === "${settings.username}")
+        {
+            return polkit.Result.YES;
+        }
+    });
+  '';
+
   services.xserver = {
     videoDrivers = [ "amdgpu" ];
     xkb = {
