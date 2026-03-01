51360d71eef1a73d9c3850503dc5d7e45cd8e87d
rhysmc190 <rhysmc190@gmail.com>
2026-03-01 23:28:06 +1100

    use curses for pinentry

--- home/gpg.nix
+++ home/gpg.nix
@@ -4,7 +4,7 @@
 
   services.gpg-agent = {
     enable = true;
-    pinentry.package = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-curses;
     defaultCacheTtl = 28800;
     maxCacheTtl = 28800;
   };
