82d0d8deccbf876e0e82672806429d0d4470e06c
rhysmc190 <rhysmc190@gmail.com>
2026-03-04 10:45:10 +1100

    fingerprint timeout 3s

--- system/sudo.nix
+++ system/sudo.nix
@@ -1,5 +1,9 @@
-_: {
+{ config, ... }:
+{
   security.sudo.extraConfig = ''
     Defaults timestamp_timeout=30
   '';
+
+  # Shorten fingerprint timeout so SSH sudo falls through to password quickly
+  security.pam.services.sudo.rules.auth.fprintd.args = [ "timeout=3" ];
 }
