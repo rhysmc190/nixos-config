1b0ed40ffa7e8d9454fd7a028ff8670f2a41aec5
rhysmc190 <rhysmc190@gmail.com>
2026-05-03 10:55:06 +1000

    flake update

--- system/printing.nix
+++ system/printing.nix
@@ -1,6 +1,7 @@
-_: {
+{ pkgs, ... }: {
   services.printing = {
     enable = true;
     startWhenNeeded = true; # socket-activate instead of starting at boot
+    drivers = [ pkgs.gutenprint ]; # Canon PIXMA MG2900 and many others
   };
 }
